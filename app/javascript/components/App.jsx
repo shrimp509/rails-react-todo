91f1a0716c61d6a9f30a3608fa9357e89ef0a93d
UncleSam <shrimp509@hotmail.com>
2022-10-05 20:41:42 +0800

    Refactor API to services

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -3,62 +3,38 @@ import React, { useState, useEffect, useRef } from 'react'
 import TodoList from './TodoList'
 import TodoAdder from './TodoAdder'
 
+import { getTodosThruApi, addTodoThruApi, removeTodoThruApi } from '../services/TodoApiService'
+
 function App() {
     const initTodos = [{
         name: 'first todo',
         status: 'created'
     }]
 
-    const updateTodosThruApi = () => {
-        fetch('http://127.0.0.1:3000/api/v1/todos')
-        .then((response) => {
-            return response.json()
-        })
-        .then((response) => {
-            setTodos(response)
-        })
-    }
-
-    const addTodoThruApi = (todo) => {
-        fetch('http://127.0.0.1:3000/api/v1/todos', {
-            method: 'POST',
-            body: JSON.stringify({
-                name: todo.name
-            }),
-            headers: new Headers({
-                'Content-Type': 'application/json'
-            })
-        })
-        .then((response) => {
-            console.log(response.json())
-        })
-        .catch(error => console.error("Error: " + error))
-    }
-
-    const removeTodoThruApi = (todo) => {
-        fetch(`http://127.0.0.1:3000/api/v1/todos/${todo.id}`, {
-            method: 'DELETE'
-        })
-        .then((response) => {
-            console.log(response.json())
-        })
-        .catch(error => console.error("Error: " + error))
-    }
-
     const [todos, setTodos] = useState(initTodos)
 
     useEffect(() => {
-        updateTodosThruApi()
+        getTodosThruApi().then((response) => {
+                setTodos(response)
+        })
     }, [])
 
     const addNewTodo = (newTodo) => {
-        addTodoThruApi(newTodo)
-        setTodos([...todos, newTodo])
+        addTodoThruApi(newTodo).then((response) => {
+            if (response.status != 200) {
+                console.log(`Add todo ${newTodo} failed`); return;
+            }
+            setTodos([...todos, newTodo])
+        })
     }
 
     const removeTodo = (todoIndex) => {
-        removeTodoThruApi(todos[todoIndex])
-        setTodos(todos.filter( (_, index) => index != todoIndex ))
+        removeTodoThruApi(todos[todoIndex]).then((response) => {
+            if (response.status != 200) {
+                console.log(`Remove todo ${todos[todoIndex]} failed`); return;
+            }
+            setTodos(todos.filter( (_, index) => index != todoIndex ))
+        })
     }
 
     return <div>
---
+++ app/javascript/services/TodoApiService.js
@@ -0,0 +1,28 @@
+
+export const getTodosThruApi = () => {
+  return fetch('http://127.0.0.1:3000/api/v1/todos')
+          .then((response) => {
+              return response.json()
+          })
+          .catch(error => console.error("Error: " + error))
+}
+
+export const addTodoThruApi = (todo) => {
+  return fetch('http://127.0.0.1:3000/api/v1/todos', {
+                method: 'POST',
+                body: JSON.stringify({
+                    name: todo.name
+                }),
+                headers: new Headers({
+                    'Content-Type': 'application/json'
+                })
+            })
+            .catch(error => console.error("Error: " + error))
+}
+
+export const removeTodoThruApi = (todo) => {
+  return fetch(`http://127.0.0.1:3000/api/v1/todos/${todo.id}`, {
+              method: 'DELETE'
+          })
+          .catch(error => console.error("Error: " + error))
+}
