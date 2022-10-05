93cb09a76ad2c8fb7dd9ef87dc6118f8f84f40ad
UncleSam <shrimp509@hotmail.com>
2022-10-05 23:30:54 +0800

    Add onBlur to updatingTodo

--- app/javascript/components/UpdatingTodo.jsx
+++ app/javascript/components/UpdatingTodo.jsx
@@ -2,15 +2,21 @@ import React, { useState, useEffect, useRef } from 'react'
 
 function UpdatingTodo(props) {
     const [todo, setTodo] = useState(props.todo.name)
+    const [newTodo, setNewTodo] = useState(props.todo.name)
 
     const onSubmitHandler = (e) => {
         e.preventDefault()
-        props.updateTodo(props.index, todo)
+        props.updateTodo(props.index, newTodo)
+        props.setUpdating(false)
+    }
+
+    const onBlurHandler = (e) => {
+        setNewTodo(todo)
         props.setUpdating(false)
     }
 
     return <form onSubmit={ onSubmitHandler } style={{ display: 'inline-block' }}>
-                <input type='text' value={todo} onChange={e => {setTodo(e.target.value)}}></input>  {/* FIXME: lose focus when typing */}
+                <input type='text' autoFocus value={newTodo} placeholder={todo} onChange={e => {setNewTodo(e.target.value)}} onBlur={onBlurHandler}></input>
             </form>
 }
 
